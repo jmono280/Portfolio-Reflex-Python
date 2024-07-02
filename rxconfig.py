178fd9c8c279f32cf4099968ff65a5fa1a4afdd8
JuanPGutiDev <jmono280@gmail.com>
2024-07-02 21:47:07 +0200

    Agregada config Vercel

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,4 @@ import reflex as rx
 config = rx.Config(
     app_name="portafolio",
     icon="devicon-json-plain",
-    frontend_port=3001,
-    api_url="http://jc4kws8.3.86.13.167.sslip.io:7000",
 )
